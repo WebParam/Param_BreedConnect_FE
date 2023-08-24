13ea98c4b9ff7cc37401481bf622eb85544e0a01
ParamNinjas <88320505+ParamNinjas@users.noreply.github.com>
2023-08-25 00:59:33 +0200

    Update to the profile

--- src/components/Questionnaires/questionaire-component.jsx
+++ src/components/Questionnaires/questionaire-component.jsx
@@ -3,21 +3,27 @@ import { useEffect, useState } from "react";
 import Layout from "../Partials/Layout";
 import './questionnaires.css'
 import questionnaires from "../../data/questionnaires.json"
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import Button from '@mui/material/Button';
+import { Button, Grid, LinearProgress, Icon } from '@mui/material';
+import { AccountCircle, Email, Phone,SourceRoundedIcon } from '@mui/icons-material';
 import PopupModal from '../popupModal/index';
 
 
 
 export default function QuestionnaireComponent() {
     const [open, setOpen] = useState(false);
-    const [modalContent, setModalContent] = useState(null);
     const [questions, setQuestions] = useState(questionnaires);
     const [index, setIndex] = useState(null);
     const [mainText, setMainText] = useState("Hi, to ensure that we create a safe community for breeders all breeders are screened and approved by Breed Connect");
-    const [selectedOption, setSelectedOption] = useState()
 
-    const [start, setStart] = useState('Start questionnaire')
+
+    const [start, setStart] = useState('Start questionnaire');
+
+    const [progress, setProgress] = useState(0); // Initial progress value
+
+  const handleButtonClick = (value) => {
+    // Update the progress bar when a button is clicked
+    setProgress(value);
+  };
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -31,23 +37,6 @@ export default function QuestionnaireComponent() {
         };
         fetchData();
     }, [questions]);
-
-    const handleClick = () => {
-        console.log("", questions[index])
-        if (index === questions.length - 1) {
-            setStart("Finish")
-            setIndex(null)
-        } else {
-            setIndex(index + 1)
-            setStart('Next');
-
-        }
-    };
-
-
-    const handleSelectChange = (event) => {
-        setSelectedOption(event.target.value);
-    };
 
     const handleOpen = (e) => {
         e.preventDefault();
@@ -63,7 +52,46 @@ export default function QuestionnaireComponent() {
 
             <div className="questionnaire-container">
                 {
-                    index === null ? <div className="header-section">{mainText}</div> : ''
+                    index === null ? 
+                    // <div className="header-section">
+                    //     {mainText}
+                       
+                    //  </div> 
+                     <div>
+                         <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <center><h1>Complete Profile</h1></center>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="outlined"
+          startIcon={<AccountCircle />}
+          onClick={() => handleButtonClick(25)}
+        >
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="outlined"
+          startIcon={<Email />}
+          onClick={() => handleButtonClick(50)}
+        >
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="outlined"
+          startIcon={<Phone />}
+          onClick={() => handleButtonClick(75)}
+        >
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <LinearProgress variant="determinate" value={progress} />
+      </Grid>
+    </Grid>
+                     </div>
+                     : ''
                 }
                 {
                     index === null ? <div className="question-section">Please complete this questionnaire to get started</div> : ""
