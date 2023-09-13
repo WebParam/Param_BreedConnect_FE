6dc703300466761d5828a7ef4a7849056c7a8eb6
ParamNinjas <88320505+ParamNinjas@users.noreply.github.com>
2023-09-13 14:27:41 +0200

    Swipes

--- src/components/Auth/Profile/index.jsx
+++ src/components/Auth/Profile/index.jsx
@@ -7,13 +7,7 @@ import IcoAdress from "./icons/IcoAdress";
 import IcoCart from "./icons/IcoCart";
 import IcoDashboard from "./icons/IcoDashboard";
 import IcoLogout from "./icons/IcoLogout";
-import IcoLove from "./icons/IcoLove";
-import IcoPassword from "./icons/IcoPassword";
-import IcoPayment from "./icons/IcoPayment";
 import IcoPeople from "./icons/IcoPeople";
-import IcoReviewHand from "./icons/IcoReviewHand";
-import IcoSupport from "./icons/IcoSupport";
-import AddressesTab from "./tabs/AddressesTab";
 import Dashboard from "./tabs/Dashboard";
 import OrderTab from "./tabs/OrderTab";
 import PasswordTab from "./tabs/PasswordTab";
@@ -22,6 +16,7 @@ import ProfileTab from "./tabs/ProfileTab";
 import ReviewTab from "./tabs/ReviewTab";
 import SupportTab from "./tabs/SupportTab";
 import WishlistTab from "./tabs/WishlistTab";
+import SwipesTab from "./tabs/SwipesTab";
 
 export default function Profile() {
   const [switchDashboard, setSwitchDashboard] = useState(false);
@@ -36,15 +31,6 @@ export default function Profile() {
     );
   }, [getHashContent]);
 
-
-  function ProfileHeader() {
-    return (
-      <div className="profile-header">
-        <img src="profile-cover.jpg" alt="Cover" className="cover-photo" />
-        <img src="profile-picture.jpg" alt="Profile" className="profile-picture" />
-      </div>
-    );
-  }
 
 
   return (
@@ -68,21 +54,6 @@ export default function Profile() {
                             Your Dashbaord
                           </span>
                         </div>
-              
-                <div className="switch-dashboard flex space-x-3 items-center">
-                  <p className="text-qgray text-base">Switch Dashboard</p>
-                  <button
-                    onClick={() => setSwitchDashboard(!switchDashboard)}
-                    type="button"
-                    className="w-[73px] h-[31px] border border-[#D9D9D9] rounded-full relative "
-                  >
-                    <div
-                      className={`w-[23px] h-[23px] bg-qblack rounded-full absolute top-[3px] transition-all duration-300 ease-in-out ${
-                        switchDashboard ? "left-[44px]" : "left-[4px]"
-                      }`}
-                    ></div>
-                  </button>
-                </div>
               </div>
               <div className="profile-wrapper w-full mt-8 flex space-x-10">
                 <div className="w-[236px] min-h-[600px] border-r border-[rgba(0, 0, 0, 0.1)]">
@@ -140,7 +111,7 @@ export default function Profile() {
                     </div>
 
                     <div className="item group">
-                      <Link to="/profile#tinder">
+                      <Link to="/profile#swipes">
                         <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
                           <span>
                           
@@ -165,88 +136,7 @@ export default function Profile() {
                       </Link>
                     </div>
 
-                    {/* <div className="item group"> */}
-                      {/* <Link to="/profile#payment">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoPayment />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Payment Method
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#order">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoCart />
-                          </span>
-                          <span className=" font-normal text-base">Order</span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#wishlist">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoLove />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Wishlist
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#address">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoAdress />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Address
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#review">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoReviewHand />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Reviews
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#password">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoPassword />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Change Password
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#support">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoSupport />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Support Ticket
-                          </span>
-                        </div>
-                      </Link>
-                    </div> */}
+                    
                     <div className="item group">
                       <Link to="/profile#profile">
                         <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
@@ -281,9 +171,9 @@ export default function Profile() {
                       <>
                         <WishlistTab />
                       </>
-                    ) : active === "address" ? (
+                    ) : active === "swipes" ? (
                       <>
-                        <AddressesTab />
+                        <SwipesTab />
                       </>
                     ) : active === "password" ? (
                       <>
---
+++ src/components/Auth/Profile/tabs/SwipesTab.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import './stylesheets/tabs.css'
+import InputCom from "../../../Helpers/InputCom";
+
+export default function SwipesTab() {
+  return (
+    <>
+      <div className="relative w-full overflow-x-auto sm:rounded-lg orders">
+        <h2>Swipes</h2>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <tbody>
+            {/* table heading */}
+            <tr className="text-base text-qgray whitespace-nowrap px-2 border-b default-border-bottom ">
+              <td className="py-4 whitespace-nowrap table-header">Name</td>
+              <td className="py-4 whitespace-nowrap table-header">Date</td>
+              <td className="py-4 whitespace-nowrap table-header">Accept</td>
+              <td className="py-4 whitespace-nowrap table-header">Reject</td>
+              <td className="py-4 whitespace-nowrap table-header"></td>
+            </tr>
+            {/* table heading end */}
+            <tr className=" border-b hover:bg-gray-50">
+              <td className="py-1">
+                <span className="text-lg text-qgray font-medium">Jane Doe</span>
+              </td>
+              <td className="px-1">
+                <span className="text-lg text-qgray font-medium">
+                  Feb 05,2021
+                </span>
+              </td>
+              <td className="px-1">
+                <span className="">
+                  <button>
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/accept.svg`}  alt="accept" className="accept-picture" />
+                  </button>
+                
+                </span>
+              </td>
+              <td className="py-1">
+              <span className="">
+                  <button>
+                  <img src={`${process.env.PUBLIC_URL}/assets/images/reject.svg`}  alt="reject" className="reject-picture" />
+                  </button>
+                
+                </span>
+              </td>
+              <td className="text-center py-1 px-2">
+                <span className="text-base text-qblack whitespace-nowrap px-2 ">
+                <InputCom
+                placeholder="Code"
+                type="text"
+                inputClasses="h-[10px]"
+              />
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
