8d23db4c72c0e1e1ca1bec7e8ce7e4543d30d157
NjinuParam <njinu@webparam.org>
2023-09-30 09:17:32 +0200

    feat(up)

--- src/components/Dashboard/Messages/messages.jsx
+++ src/components/Dashboard/Messages/messages.jsx
@@ -16,13 +16,13 @@ export default function Messages() {
 
 async function payWithPaystack(purchaseRequestId) {
 
-  const res = await CreatePaymentLink("6511bf7e4332f3f75ec0a0a4");
+  const res = await CreatePaymentLink("651546b22136b9524a35d590");
   debugger;
 
 }
 
 async function pay(paymentLinkId){
-  const res = await GenerateLink("6511e2ddc0243f92bc903a0b"
+  const res = await GenerateLink("651547a02136b9524a35d594"
   
   );
   debugger;
